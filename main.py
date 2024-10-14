518c39cc6e8252a754597def2cbdac74fd3a8d1f
Kashish1702 <88196390+Kashish1702@users.noreply.github.com>
2024-10-14 15:09:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     result = 3*4
-    return f"Hello, World! Result of addition: {result}, this is flask app"
+    return f"Hello, World! Result of multiplication: {result}, this is flask app"
 
 if __name__ == "__main__":
     app.run(debug=True)
