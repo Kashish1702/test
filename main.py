08030df8ac9b3910e59e02699bc235ae3dcb6fb5
Kash087 <kashish87@antiersolutions.com>
2024-10-14 13:07:32 +0530

    testing

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     result = sum([3, 4])
-    return f"Hello, World! Result of addition: {result}"
+    return f"Hello, World! Result of addition: {result}, this is flask app"
 
 if __name__ == "__main__":
     app.run(debug=True)
