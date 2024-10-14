32f08969447b19587781c6a81e91c2ed1feff2f9
Kashish1702 <88196390+Kashish1702@users.noreply.github.com>
2024-10-14 15:08:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    result = sum([3, 4])
+    result = 3*4
     return f"Hello, World! Result of addition: {result}, this is flask app"
 
 if __name__ == "__main__":
